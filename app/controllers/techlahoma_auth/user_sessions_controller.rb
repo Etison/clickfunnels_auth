791cf215c4c4b1a28fa645b49e8d546811270639
Jeremy Green <jeremy@octolabs.com>
2016-02-10 22:18:16 -0600

    Capture name. And version bump.

--- app/controllers/techlahoma_auth/user_sessions_controller.rb
+++ app/controllers/techlahoma_auth/user_sessions_controller.rb
@@ -14,6 +14,7 @@ class TechlahomaAuth::UserSessionsController < TechlahomaAuth::ApplicationContro
       user.id = omniauth['uid']
     end
     user.email = omniauth['info']['email'] if user.respond_to?(:email)
+    user.email = omniauth['info']['name']  if user.respond_to?(:name)
     user.save
 
     session[:user_id] = user.id
