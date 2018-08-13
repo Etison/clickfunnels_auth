bff7620a66bd2ca4037fc3ff73ec4e9330b9bc1b
Jeremy Green <jeremy@octolabs.com>
2018-08-13 16:02:37 -0500

    Remove comented out code

--- app/controllers/clickfunnels_auth/application_controller.rb
+++ app/controllers/clickfunnels_auth/application_controller.rb
@@ -3,14 +3,6 @@ module ClickfunnelsAuth
 
     include ClickfunnelsAuth::Helper
 
-    #before_filter :check_cookie
-    #def check_cookie
-      #if !cookie_valid?
-        #session[:user_id] = nil
-      #end
-    #end
-
-
   end
 end
 
